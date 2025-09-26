b7b20a396bc9928106d6713c3ffb0ef1cdb8ffab
alexa <alexanderanufriev111@gmail.com>
2025-09-26 20:39:50 +0300

    my marks

---
+++ footer.php
@@ -0,0 +1,8 @@
+    <footer>
+        
+    </footer>
+    <!-- SVG sprite injected once per page -->
+    <?php include __DIR__ . '/assets/sprite.svg'; ?>
+  </body>
+</html>
+
---
+++ header.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="assets/css/style.css">
+</head>
+<body>
+<header class="dark-block">
+    <div class="container">
+        <div class="top-part">
+            <svg width="182" height="45">
+                <use href="#logo"></use>
+            </svg>
+            <div class="support-block">
+                <div class="language-codes">
+                    <a href="#">1</a>
+                    <a href="#">1</a>
+                    <a href="#">1</a>
+                </div>
+                <a href="#" class="btn btn-transparent">Jautājums?</a>
+                <a href="#" class="btn btn-matte">+371 25912321</a>
+            </div>
+        </div>
+        <div class="bottom-part">
+            <ul class="main-menu">
+                <li><a href="#">page</a></li>
+                <li><a href="#">page</a></li>
+                <li><a href="#">page</a></li>
+                <li><a href="#">page</a></li>
+                <li><a href="#">page</a></li>
+            </ul>
+
+        </div>
+    </div>
+</header>
+
