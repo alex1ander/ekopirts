da15aee5e2c50b08891bc69ed17a77eb11cffea2
alexa <alexanderanufriev111@gmail.com>
2025-09-29 21:37:12 +0300

    work

---
+++ components/product-card.php
@@ -0,0 +1,28 @@
+<div class="product-card">
+    <div class="product-image">
+        <img src="/images/product.png" alt="">
+    </div>
+
+    <div class="product-content">
+        <h4 class="product-name">Pirts Muca (ovālā) 6m + Terase + Kubls</h4>
+        <div class="product-info">
+            <p class="product-description">Pirts Muca (ovālā) 6m+Terase+ Kubls.</p>
+        </div>
+
+        <div class="product-price">
+            <span class="old-price">€ 5990</span>
+            <span class="current-price">€ 4990</span>
+        </div>
+
+        <a href="#" class="product-more">Uzzināt vairāk</a>
+
+        <div class="add-order">
+            <div class="order-text">Noformēt pasūtījumu</div>
+            <div class="icon">
+                <svg width="33" height="33">
+                    <use href="#whatsapp"></use>
+                </svg>
+            </div>
+        </div>
+    </div>
+</div>
--- components/products-slider.php
+++ components/products-slider.php
@@ -1,4 +1,4 @@
-<sections class="products-slider">
+<section class="products-slider">
     <div class="container">
         <div class="products-head">
             <div class="title-block">
@@ -12,5 +12,36 @@
             </ul>
             <a href="#" class="products-link">View All Products</a>
         </div>
+
+        <div class="proudcts-body">
+            <div class="swiper products-slider-swiper">
+                <div class="swiper-wrapper">
+                        <?php for($i = 0; $i < 8; $i++): ?>
+                            <div class="swiper-slide">
+                                <?php include 'product-card.php'; ?>
+                            </div>
+                        <?php endfor; ?>
+                </div>
+                <!-- <div class="products-slider-button-next"></div>
+                <div class="products-slider-button-prev"></div>
+                <div class="products-slider-pagination"></div> -->
+            </div>
+        </div>
     </div>
-</sections>
+</section>
+
+
+<script>
+    var swiper = new Swiper(".products-slider-swiper", {
+      slidesPerView: 4,
+      spaceBetween: 20,
+      pagination: {
+        el: ".products-slider-pagination",
+        clickable: true,
+      },
+      navigation: {
+        nextEl: ".products-slider-button-next",
+        prevEl: ".products-slider-button-prev",
+      },
+    });
+</script>
