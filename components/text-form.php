fd12e9e2b3c93182c9a4ae2469dbc0d730f62256
alexa <alexanderanufriev111@gmail.com>
2025-10-09 21:39:55 +0300

    upd

---
+++ components/text-form.php
@@ -0,0 +1,43 @@
+<section class="text-form-section">
+    <div class="container">
+        <div class="text-form">
+            <div class="text-area">
+
+                <div class="title-block">
+                    <h2 class="products-title">Mūsu produkcija</h2>
+                </div>
+
+                <div class="text">
+                    Par produktu piegādi vienojamies ar katru klientu individuāli.
+
+                        Vienojoties ar klientu, nodrošinām:
+
+                        piegādi uz klientam vēlamo vietu, izmantojot mūsu transportu
+                        piegāde 0,70 eur/km;
+                        piegādi un uzstādīšanu;
+                        iespēju pašiem saņemt pasūtījumu pie mums.
+
+                        Katram klientam individuāli ļaujam izvēlēties sev ērtāko produktu saņemšanas veidu.
+
+                        Uzstādīšana
+
+                        Pirms pasūtījuma uzstādīšanas dodamies pie klienta uz vietu, kur plānots uzstādīt pirti, kublu, lapeni, dārza māju, terasi vai bērnu laukumu, lai kopā ar klientu vienotos par uzstādīšanas procesu un izvēlētos konkrētajai vietai labāko risinājumu.
+                         
+                </div>
+
+            </div>
+            <div class="form-area">
+                <form action="#">
+                    <input type="text" placeholder="Vārds, Uzvārds">
+                    <input type="text" placeholder="Vārds, Uzvārds">
+                    <input type="text" placeholder="Vārds, Uzvārds">
+
+                    <textarea name="" id=""></textarea>
+
+                    <button href="#" class="btn">Pasūtīt</button>
+
+                </form>
+            </div>
+        </div>
+    </div>
+</section>
---
+++ components/text.php
@@ -0,0 +1,23 @@
+<section class="text-section">
+    <div class="container">
+        <div class="text">
+            <p>Pēc pasūtījuma izgatavojam gan mucveida kempinga mājas, gan klasiskās kempinga mājas, kā arī piedāvājam jau gatavas tipveida kempinga mājas. Veicam arī kempinga māju piegādi un uzstādīšanu, nodrošinot pilnu servisu.</p>
+
+            <p>Kempinga mājas standarta aprīkojumā ietilpst:</p>
+
+            <ul>
+                <li>stikla durvis un logi, kā arī koka ārdurvis;</li>
+                <li>galds un paceļami soli;</li>
+                <li>plaukti;</li>
+                <li>ventilācija, apgaismojums un elektroinstalācija;</li>
+                <li>ieejas pakāpiens/slieksnis;</li>
+                <li>terase;</li>
+                <li>trepes;</li>
+                <li>bitumena šindeļa jumts.</li>
+            </ul>
+
+            <p>Mājas komplektācija var atšķirties atkarībā no izvēlētā kempinga mājas tipa (mucveida vai karkasa kempinga māja).
+    Mājas iespējams arī siltināt, kā arī komplektāciju, tehnisko un dizaina risinājumu iespējams pielāgot katra klienta individuālajām vēlmēm.</p>
+        </div>
+    </div>
+</section>
